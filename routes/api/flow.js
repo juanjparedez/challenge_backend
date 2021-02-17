2c9e6d625c17b5453041fecfdea59ad8127f3905
Juan Paredez <juanjparedez@gmail.com>
2021-02-17 19:07:31 -0300

    Api routes added

--- routes/api/flow.js
+++ routes/api/flow.js
@@ -18,8 +18,29 @@ router.get('/amount', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
+	if (req.body.category != 'Income') {
+		req.body.amount = -req.body.amount
+	}
 	const transaction = await Transaction.create(req.body)
 	res.json(transaction)
+})
+
+router.put('/:transactionID', async (req, res) => {
+	await Transaction.update(req.body, {
+		where: { id: req.params.transactionID },
+	})
+	let updated = await Transaction.findByPk(req.params.transactionID)
+	res.json(updated)
+})
+
+router.delete('/:transactionID', async (req, res) => {
+	await Transaction.destroy({
+		where: { id: req.params.transactionID },
+	})
+
+	res.json({
+		success: `Se ha borrado de la tabla el elemento con ID: ${req.params.transactionID}`,
+	})
 })
 
 module.exports = router
